79cf225e868509e4e5174a55804e9ae7309474ae
ART1 <ART1@HOME-PC>
2024-05-18 18:16:11 +0300

    Добавьте файлы проекта.

---
+++ KURSOVOI_PROECT_KURS3/Form2.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace KURSOVOI_PROECT_KURS3
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            файлToolStripMenuItem = new ToolStripMenuItem();
+            сохранитьToolStripMenuItem = new ToolStripMenuItem();
+            выйтиToolStripMenuItem = new ToolStripMenuItem();
+            справкаToolStripMenuItem = new ToolStripMenuItem();
+            помощьToolStripMenuItem = new ToolStripMenuItem();
+            label1 = new Label();
+            monthCalendar1 = new MonthCalendar();
+            button1 = new Button();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { файлToolStripMenuItem, справкаToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(1080, 28);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            файлToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { сохранитьToolStripMenuItem, выйтиToolStripMenuItem });
+            файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            файлToolStripMenuItem.Size = new Size(59, 24);
+            файлToolStripMenuItem.Text = "Файл";
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            сохранитьToolStripMenuItem.Size = new Size(166, 26);
+            сохранитьToolStripMenuItem.Text = "Сохранить";
+            // 
+            // выйтиToolStripMenuItem
+            // 
+            выйтиToolStripMenuItem.Name = "выйтиToolStripMenuItem";
+            выйтиToolStripMenuItem.Size = new Size(166, 26);
+            выйтиToolStripMenuItem.Text = "Выйти";
+            выйтиToolStripMenuItem.Click += выйтиToolStripMenuItem_Click;
+            // 
+            // справкаToolStripMenuItem
+            // 
+            справкаToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { помощьToolStripMenuItem });
+            справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
+            справкаToolStripMenuItem.Size = new Size(81, 24);
+            справкаToolStripMenuItem.Text = "Справка";
+            // 
+            // помощьToolStripMenuItem
+            // 
+            помощьToolStripMenuItem.Name = "помощьToolStripMenuItem";
+            помощьToolStripMenuItem.Size = new Size(152, 26);
+            помощьToolStripMenuItem.Text = "Помощь";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 78);
+            label1.Name = "label1";
+            label1.Size = new Size(191, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Выберите диапозон дней:";
+            // 
+            // monthCalendar1
+            // 
+            monthCalendar1.Location = new Point(12, 107);
+            monthCalendar1.Name = "monthCalendar1";
+            monthCalendar1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 326);
+            button1.Name = "button1";
+            button1.Size = new Size(125, 29);
+            button1.TabIndex = 4;
+            button1.Text = "Выбрать день";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1080, 498);
+            Controls.Add(button1);
+            Controls.Add(monthCalendar1);
+            Controls.Add(label1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form2";
+            Text = "Form2";
+            FormClosing += Form2_FormClosing;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem файлToolStripMenuItem;
+        private ToolStripMenuItem сохранитьToolStripMenuItem;
+        private ToolStripMenuItem выйтиToolStripMenuItem;
+        private ToolStripMenuItem справкаToolStripMenuItem;
+        private ToolStripMenuItem помощьToolStripMenuItem;
+        private Label label1;
+        private MonthCalendar monthCalendar1;
+        private Button button1;
+    }
+}
---
+++ KURSOVOI_PROECT_KURS3/Form2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace KURSOVOI_PROECT_KURS3
+{
+    public partial class Form2 : Form
+    {
+        int c = 0; //переключатель для выхода из программы
+        public Form2()
+        {
+            InitializeComponent();
+        }
+ 
+        private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (c == 0)
+            {
+                Application.OpenForms[0].Show();
+            }
+        }
+
+        private void выйтиToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            c = 1;
+            Application.OpenForms[0].Show();
+            this.Close();
+        }
+
+        class Raport
+        {
+            public DateTime StartWeek { get; set; }
+            public DateTime EndWeek;
+            public Raport(DateTime StartWeek)
+            {
+                this.StartWeek = StartWeek;
+                EndWeek = StartWeek.AddDays(6);
+            }
+
+        }
+    }
+}
