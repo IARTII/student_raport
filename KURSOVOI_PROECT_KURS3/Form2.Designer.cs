3c87ae0e4faa5694f70cd3cf835a4baf05f4a41c
mahlaevartur@gmail.com <mahlaevartur@gmail.com>
2024-05-27 20:10:37 +0300

    версия 0.5

--- KURSOVOI_PROECT_KURS3/Form2.Designer.cs
+++ KURSOVOI_PROECT_KURS3/Form2.Designer.cs
@@ -36,7 +36,13 @@
             помощьToolStripMenuItem = new ToolStripMenuItem();
             label1 = new Label();
             monthCalendar1 = new MonthCalendar();
-            button1 = new Button();
+            listBox1 = new ListBox();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            button2 = new Button();
+            button3 = new Button();
             menuStrip1.SuspendLayout();
             SuspendLayout();
             // 
@@ -46,7 +52,7 @@
             menuStrip1.Items.AddRange(new ToolStripItem[] { файлToolStripMenuItem, справкаToolStripMenuItem });
             menuStrip1.Location = new Point(0, 0);
             menuStrip1.Name = "menuStrip1";
-            menuStrip1.Size = new Size(1080, 28);
+            menuStrip1.Size = new Size(745, 28);
             menuStrip1.TabIndex = 1;
             menuStrip1.Text = "menuStrip1";
             // 
@@ -60,13 +66,14 @@
             // сохранитьToolStripMenuItem
             // 
             сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
-            сохранитьToolStripMenuItem.Size = new Size(166, 26);
+            сохранитьToolStripMenuItem.Size = new Size(224, 26);
             сохранитьToolStripMenuItem.Text = "Сохранить";
+            сохранитьToolStripMenuItem.Click += сохранитьToolStripMenuItem_Click;
             // 
             // выйтиToolStripMenuItem
             // 
             выйтиToolStripMenuItem.Name = "выйтиToolStripMenuItem";
-            выйтиToolStripMenuItem.Size = new Size(166, 26);
+            выйтиToolStripMenuItem.Size = new Size(224, 26);
             выйтиToolStripMenuItem.Text = "Выйти";
             выйтиToolStripMenuItem.Click += выйтиToolStripMenuItem_Click;
             // 
@@ -86,33 +93,92 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(12, 78);
+            label1.Location = new Point(12, 42);
             label1.Name = "label1";
-            label1.Size = new Size(191, 20);
+            label1.Size = new Size(249, 20);
             label1.TabIndex = 2;
-            label1.Text = "Выберите диапозон дней:";
+            label1.Text = "Выберите стартовый день недели:";
+            label1.Click += label1_Click;
             // 
             // monthCalendar1
             // 
-            monthCalendar1.Location = new Point(12, 107);
+            monthCalendar1.Location = new Point(12, 71);
             monthCalendar1.Name = "monthCalendar1";
             monthCalendar1.TabIndex = 3;
+            monthCalendar1.DateChanged += monthCalendar1_DateChanged;
             // 
-            // button1
+            // listBox1
             // 
-            button1.Location = new Point(12, 326);
-            button1.Name = "button1";
-            button1.Size = new Size(125, 29);
-            button1.TabIndex = 4;
-            button1.Text = "Выбрать день";
-            button1.UseVisualStyleBackColor = true;
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(296, 192);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(433, 184);
+            listBox1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(296, 42);
+            label2.Name = "label2";
+            label2.Size = new Size(172, 20);
+            label2.TabIndex = 6;
+            label2.Text = "Введите ФИО старосты:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(296, 71);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(305, 27);
+            textBox1.TabIndex = 7;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(296, 112);
+            label3.Name = "label3";
+            label3.Size = new Size(172, 20);
+            label3.TabIndex = 8;
+            label3.Text = "Введите ФИО куратора:";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(296, 135);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(305, 27);
+            textBox2.TabIndex = 9;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(607, 71);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 10;
+            button2.Text = "Добавить";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(607, 134);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 11;
+            button3.Text = "Добавить";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
             // 
             // Form2
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1080, 498);
-            Controls.Add(button1);
+            ClientSize = new Size(745, 392);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(listBox1);
             Controls.Add(monthCalendar1);
             Controls.Add(label1);
             Controls.Add(menuStrip1);
@@ -120,6 +186,7 @@
             Name = "Form2";
             Text = "Form2";
             FormClosing += Form2_FormClosing;
+            Load += Form2_Load;
             menuStrip1.ResumeLayout(false);
             menuStrip1.PerformLayout();
             ResumeLayout(false);
@@ -135,6 +202,12 @@
         private ToolStripMenuItem помощьToolStripMenuItem;
         private Label label1;
         private MonthCalendar monthCalendar1;
-        private Button button1;
+        private ListBox listBox1;
+        private Label label2;
+        private TextBox textBox1;
+        private Label label3;
+        private TextBox textBox2;
+        private Button button2;
+        private Button button3;
     }
 }
--- KURSOVOI_PROECT_KURS3/Form2.cs
+++ KURSOVOI_PROECT_KURS3/Form2.cs
@@ -8,42 +8,148 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.OleDb;
+using Microsoft.Office.Interop.Excel;
 
 namespace KURSOVOI_PROECT_KURS3
 {
     public partial class Form2 : Form
     {
         int c = 0; //переключатель для выхода из программы
+        string startWeekDate;
+        string endWeekDate;
         public Form2()
         {
             InitializeComponent();
         }
- 
+
         private void Form2_FormClosing(object sender, FormClosingEventArgs e)
         {
             if (c == 0)
             {
-                Application.OpenForms[0].Show();
+                System.Windows.Forms.Application.OpenForms[0].Show();
             }
         }
 
         private void выйтиToolStripMenuItem_Click(object sender, EventArgs e)
         {
             c = 1;
-            Application.OpenForms[0].Show();
+            System.Windows.Forms.Application.OpenForms[0].Show();
             this.Close();
         }
 
         class Raport
         {
             public DateTime StartWeek { get; set; }
-            public DateTime EndWeek;
+            public DateTime EndWeek { get; set; }
             public Raport(DateTime StartWeek)
             {
                 this.StartWeek = StartWeek;
                 EndWeek = StartWeek.AddDays(6);
             }
 
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Выбранные данные:");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                string currentItem = listBox1.Items[i].ToString();
+
+                if (currentItem.Contains("Старосты"))
+                {
+                    listBox1.Items.RemoveAt(i);
+                    i--; // Уменьшаем счетчик, так как после удаления элементов сдвигаются
+                }
+            }
+            listBox1.Items.Add("ФИО Старосты: " + textBox1.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                string currentItem = listBox1.Items[i].ToString();
+
+                if (currentItem.Contains("Куратора"))
+                {
+                    listBox1.Items.RemoveAt(i);
+                    i--; // Уменьшаем счетчик, так как после удаления элементов сдвигаются
+                }
+            }
+            listBox1.Items.Add("ФИО Куратора: " + textBox2.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                string currentItem = listBox1.Items[i].ToString();
+
+                if (currentItem.Contains("Куратора"))
+                {
+                    listBox1.Items.RemoveAt(i);
+                    i--; // Уменьшаем счетчик, так как после удаления элементов сдвигаются
+                }
+            }
+            listBox1.Items.Add("Выбранная неделя:  " + monthCalendar1);
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                string currentItem = listBox1.Items[i].ToString();
+
+                if (currentItem.Contains("неделя"))
+                {
+                    listBox1.Items.RemoveAt(i);
+                    i--; // Уменьшаем счетчик, так как после удаления элементов сдвигаются
+                }
+            }
+            DateTime dt1 = Convert.ToDateTime(e.Start.ToLongDateString());
+            DateTime dt2 = dt1.AddDays(5);
+            startWeekDate = dt1.Day + "." + dt1.Month + "." + dt1.Year;
+            endWeekDate = dt2.Day + "." + dt2.Month + "." + dt2.Year;
+            listBox1.Items.Add(String.Format("Выбранная неделя: " + dt1.Day + "." + dt1.Month + "." + dt1.Year + " - " + dt2.Day + "." + dt2.Month + "." + dt2.Year));
+        }
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string fileOutputStream;
+            SaveFileDialog SFD = new SaveFileDialog();
+            SFD.Filter = "XLSX (*.xlsx)|*.xlsx|XLS (*.xls)|*.xls";
+
+            if (SFD.ShowDialog() == DialogResult.OK)    
+            {
+                File.Copy(@"C:\Users\ART1\source\repos\KURSOVOI_PROECT_KURS3\KURSOVOI_PROECT_KURS3\bin\Debug\net8.0-windows\шаблон_рапортичка.xlsx", SFD.FileName);
+                fileOutputStream = SFD.FileName;
+                MessageBox.Show("Файл успешно сохранен.", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                
+
+                Microsoft.Office.Interop.Excel.Application excelApp = new Microsoft.Office.Interop.Excel.Application();
+                Workbook excelWorkbook = excelApp.Workbooks.Open(fileOutputStream);
+                Worksheet worksheet = excelWorkbook.Sheets[1];
+
+                //2 12
+                worksheet.Cells[2, 22].Value += textBox1.Text;
+                worksheet.Cells[2, 27].Value += textBox2.Text;
+                worksheet.Cells[2, 12].Value += startWeekDate;
+                worksheet.Cells[2, 15].Value += endWeekDate;
+
+                excelWorkbook.Save();
+                excelWorkbook.Close();
+                excelApp.Quit();
+            }
         }
     }
 }
